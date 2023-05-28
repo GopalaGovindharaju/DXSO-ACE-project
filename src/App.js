937dc21cd6b5533442a2a634414215301a6b5bb9
Arthiclaks <chitraraveendran.com>
2023-05-28 18:41:14 +0530

    choose file option added!

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import {Routes, Route} from 'react-router-dom';
 import Table from './component2/table2';
 import Leftcolumn from './component3/leftbox';
 import Lbox from './component4/lbox';
+import Browse from './component0/browse';
 
 function App() {
   return (
     <div className='App'>
     <Routes>
-     <Route path='/' element={<Order/>}></Route>
+      <Route path='/' element={<Browse/>}></Route>
+     <Route path='/planner' element={<Order/>}></Route>
      <Route path='operator' element={<Table/>}></Route>
      <Route path='inspection' element={<Leftcolumn/>}></Route>
      <Route path='inspector' element={<Lbox/>}></Route>
