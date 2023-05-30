be878b323cf2cf6a88a29659c42d341a00ae03a5
Arthiclaks <chitraraveendran.com>
2023-05-30 19:06:13 +0530

    backend changes

--- backend/planner/admin.py
+++ backend/planner/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Bom
+"""from .models import Bom
 
 class BomAdmin(admin.ModelAdmin):
     list_display = ('SL.NO', 'Sub_Assy_part_No', 'REV_NO', 'Child_Part_No', 'Part_Name', '801033002', 'Quantity', '801033007', '801033001S', '801033006S', '801032566', '801032563', '801032568', '801032569', '801032562', '801032539', '801032537', '801032531', '801032529', '801032525', '801032519', '801032510', '801033008', '801032573', '801032595', '801032608', '801032511', '801032589', '801032614S', '801032612S', '801032598', '801032618', '801032603', '801023089', '801032597', '801032627S', '801032638', '801032625', '801032619', '801032601', '801032652S', '801032651S', '801032656S', '801032662', '801032658S', '801032665', '801032697S', '801032605S', '801032632S', '801032694', '801212223', '801032719S', '801032787S - WIRED RACK , 93E 500KVA Busbar Panel, O/S', '801032641S - WIR RACK,93E 200KVA MBS INT O/P TRX- O/S', '801032630S - WIRED RACK, 93E 160KVA MBS Int Trx-O/S', '801033052S - WR, 93E 160KVA MBS, I/P Int Trx, K13, O/', '801033054S - WIR RCK,93E100kVA MBS INT TRX Cu,K13,O/S', '801033056S - WR ,93E 160KVA MBS,I/P Trx,W/t Trx MBS', '801033057S - WR 93E, 80 kVA, MBS, IT42 TRX MBS Datson', '801033059S - WR,93E 160KVA MBS,I/P Trx,W/t Trx MBS OS', 'Surface_Finish', 'Material', 'Qty_In_Drawing', 'Thickness', 'Width', 'Length', 'Blank_Wt', 'Powder_Coating_Area', 'Powder_Usage_in_Kgs', 'Assembly/Sub_part', 'commodity', 'Source', 'Wire_Rack/Non_Wire Rack_Parts', 'Revion_No', 'Revision_Details', 'Tapping', 'No.Of_Bends', 'Clinching /Riveting', 'Welding', 'METAL_STAMP ', 'POWDER_COATING', 'Unnamed: 81', 'Unnamed: 82', 'Unnamed: 83')
 
 # Register your models here.
 admin.site.register(Bom, BomAdmin)
+"""
--- backend/planner/views.py
+++ backend/planner/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import pandas as pd
 
-# Create your views here.
+@csrf_exempt  # To disable CSRF protection for simplicity in this example
+def upload_file(request):
+    if request.method == 'POST' and request.FILES.get('file'):
+        uploaded_file = request.FILES['file']
+        df = pd.read_excel(uploaded_file, sheet_name="Sheet1", header = 2).fillna(0)
+        column_names = df.columns.tolist()
+        return HttpResponse(column_names)
+    else:
+        return HttpResponse('No file received.')
