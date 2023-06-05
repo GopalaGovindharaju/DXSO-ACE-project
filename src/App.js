71d6ae7e1e0e3a82c3ad1fd2347808fe808d4aa6
gopi <gopivelraj7169@gmail.com>
2023-06-05 19:20:07 +0530

    doneee

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes, Route} from 'react-router-dom';
 import Comp from './bomviewer_gopi/comp_main';
 import Component3 from './planner_chitra/comp3_main';
+import Cust_asset from './assest_harish/custasset.js';
 
 function App() {
   return (
     <div className='App'>
+    
     <Routes>
       <Route path='/' element={<Comp/>}></Route>
       <Route path='/planner' element={<Component3/>}></Route>
+      <Route path='/cus' element={<Cust_asset/>}></Route>
      </Routes>
      </div>
   );
