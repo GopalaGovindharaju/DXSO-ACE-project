18e2a9115c961ac3c203803741244abdb9c1f9f3
Gokulgithu <spgjgokul03@gmail.com>
2023-06-08 11:49:10 +0530

    Home Updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Routes, Route} from 'react-router-dom';
 import Comp from './bomviewer_gopi/comp_main';
-
+import First from './home_gokul/login_home';
 function App() {
   return (
     <div className='App'>
     <Routes>
-      <Route path='/' element={<Comp/>}></Route>
+      <Route path='/' element={<First/>}></Route>
      </Routes>
      </div>
   );
