04dc390365c1bfc5d6901622b000f27d73f1d3e5
gopi <gopivelraj7169@gmail.com>
2023-06-12 14:48:18 +0530

    pulling

--- src/inspection/Banner.js
+++ src/inspection/Banner.js
@@ -3,16 +3,17 @@ import { useState,useEffect } from 'react'
 import CurrentTime from '../bomviewer_gopi/time.js'
 import CurrentDate from './datecomp.js'
 import '../bomviewer_gopi/component.css'
+import Icon from './insp_icon.js'
 
 function Banner3(){
   
   return (
-      <div class="row text-white bg-gradient text-center custom-font rounded">
-        <div class="col-8 bhead">
+      <div class="row text-white bg-gradient text-center custom-font ">
+        <div class="col-4 bhead">
           <h1>Bending Inspection Form - Operator</h1>
         </div>
-
-        <div class="col-4 datetime">
+        <Icon/>
+        <div class="col-3 datetime">
         <div class="row">
         <CurrentDate/>
         </div>
